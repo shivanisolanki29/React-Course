06527f333b297b49ae29baaba601385ca4484245
shivani_simpleDev <shivi.solanki29@gmail.com>
2025-01-13 21:00:04 +0100

    ep-08 part-2,3,create state variable and update state variable

--- src/Components/User.js
+++ src/Components/User.js
@@ -1,7 +1,13 @@
+import { useState } from "react";
+
 // const User = (props) => {
 const User = ({ name, location }) => {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="user-card">
+      <button onClick={() => setCount(count + 1)}>Count: {count}</button>
+      <p>{count}</p>
       <h3>Name: {name}</h3>
       <h4>Location: {location}</h4>
       <h5>contact: 0354872746</h5>
--- src/Components/UserClass.js
+++ src/Components/UserClass.js
@@ -3,13 +3,30 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state={
+      count:0,
+       count2:1
+    }
   }
 
   render() {
     const { name, location } = this.props;
+    const {count,count2}=this.state
 
     return (
       <div className="user-card">
+        <button
+          onClick={() => {
+            this.setState({
+              count: this.state.count + 1,
+              count2: this.state.count2 * 2,
+            });
+          }}
+        >
+          Count :{count}
+        </button>
+        <h5>Count: {count2}</h5>
         <h4 className="from">From Class</h4>
         <h2>Name: {name}</h2>
         <h4>Location: {location}</h4>
